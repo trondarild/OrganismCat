8b5800be56d466cf6c9338b2500da22c92ce6a4f
trondarild <trondarild@gmail.com>
2025-12-10 17:12:08 +0100

    add societal level

---
+++ code/societallevel.jl
@@ -0,0 +1,4 @@
+# TODO model societal structures and dynamics and individual agents interactions with societal institutions etc
+# objects are social institutions, norms, roles, etc.
+# morphisms are interactions between individuals and societal structures - e.g., compliance, enforcement, adaptation
+# also interactions between societal structures themselves - e.g., policy changes, cultural shifts, etc.
